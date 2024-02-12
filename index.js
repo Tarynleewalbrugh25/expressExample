77bec80cc0a64eb4998e0c1eb23b18bf9bff75e6
Tarynleewalbrugh25 <walbrught@gmail.com>
2024-02-12 14:46:17 +0200

    Dates

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 import express from 'express'
+import path from 'path'
 
 // create an express app
 const app = express()
+const router = express.Router()
+app.use(
+    router,
+    express.static('./static')
+    )
 const port = +process.env.PORT || 1000
 
+
 //router
-app.get('/', (req, res)=>{
-    res.json({
-        status: res.statusCode,
-        msg: 'you are home'
-    })
+router.get('^/$|/ express', display, (req, res)=>{
+    res.status(200).sendFile(path.resolve('./static/html/index.html'))
 })
-app.get('/about', (req, res)=>{
+
+
+//middleware
+function display(req, res, next) {
+    console.log("Hello There");
+    next()
+}
+
+router.get('/about', (req, res)=>{
     res.json({
         status: res.statusCode,
         msg: 'About Page'
     })
 })
 
-app.all('*', (req, res)=>{
-    res.json({
-        status: 404,
-        msg: '404 page'
-    })
-})
+// app.all('*', (req, res)=>{
+//     res.json({
+//         status: 404,
+//         msg: '404 page'
+//     })
+// })
 app.listen(port)
+
+
